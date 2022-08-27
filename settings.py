c267f96f74f8c3459e80ca08b8b4406f5df73436
vovanchubenko <112155197+vovanchubenko@users.noreply.github.com>
2022-08-27 03:58:30 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,20 @@
+
+token = "5562089309:AAHLpKp3-qO1UrZFZAhoq_U12CluQPwJFeM"
+
+botname = 'subcoinua_bot'
+
+admins = [944678884] #админы бота через запятую
+
+LITTLE_SUBCOIN_TO_GET_SUBS = 5 #минимум сабкоинов для раскрутки канала
+
+ref_count = 2 #реферальный бонус (сабкоины)
+
+REF_BONUS = 2
+
+FINE_FOR_UNSUBSCRIBING = -5 #Число должно быть отрицательным и целым
+
+SUBSCRIPTION_TERM = 7 #Какое время юзер должен оьязательно быть подписан на канал?
+
+LINK_TO_INTRODUCTION_AND_RULES = 'https://telegra.ph/Vvedenie-v-bota-a-takzhe-pravila-08-27' #ссылка на правила и инструкцию
+
+LINK_TO_CHAT_OF_BOT = 'https://t.me/+K5TjcbQSWVY2NTcy' #ссылка на чат
